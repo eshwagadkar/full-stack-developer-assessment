08af194eeebb51b639b32fc2d19dfe728422fa5a
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-02 12:18:10 +0530

    tailwind config to extract styles from .hbs file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './docs/*.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/*.hbs', './docs/*.html'],
   theme: {
     extend: {},
   },
