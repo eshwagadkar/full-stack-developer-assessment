1f22054aaa2d9bd9be33d39b6d4bda942880c19a
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-04 13:12:12 +0530

    init aside component

--- src/ui/layouts/Aside.jsx
+++ src/ui/layouts/Aside.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Image from '../components/Image.jsx'
 
-export default () => {
-    return <aside>
-        <h2>Discover the world's top Designers & Creatives.</h2>
-        <Image />
-    </aside>
+export default ({...props}) => {
+    return <aside {...props}></aside>
 }
