5e5963ed0d08fa694c25127c73d73003bd300f63
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-04 12:17:14 +0530

    init custom grid layout and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         'logo' : ['"boucherie-cursive"', 'cursive']
       }
     },
+    colors: {
+      // Custom colors 
+      customColor: '#FFD365',
+      logoColor: '#A08243',
+      headingColor: '#7F6024',
+      footerColor: '#8C681F',
+      pinkShade: '#E94B8B'
+    },
+    gridTemplateRows: {
+      'main-layout': 'repeat(3, minmax(min-content, max-content))', 
+    },
   },
   plugins: [],
 }
