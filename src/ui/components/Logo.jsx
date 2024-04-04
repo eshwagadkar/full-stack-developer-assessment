b50e2c7c98e5a8bd6495bf8a97132764ec5eb500
eshwagadkar <eshwagadkar007@gmail.com>
2024-04-04 13:13:11 +0530

    basic styling for the logo component

--- src/ui/components/Logo.jsx
+++ src/ui/components/Logo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default () => {
-    return <a href='#'><h1 className='font-logo'>dribble</h1></a>
+    return <a href='#'><h1 className='font-logo bg-customColor text-4xl text-center text-logoColor py-4'>dribble</h1></a>
 }
